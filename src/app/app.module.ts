40c98f9c3b44f22a57ce0f2d55876140e8b69ca0
Chamakura Ramana Reddy <ramana1015@gmail.com>
2019-06-12 14:49:09 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { NewsEventsComponent } from './newsEvents/newsEvents.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      AboutComponent,
+      ProgramsComponent,
+      NewsEventsComponent,
+      GalleryComponent,
+      ContactComponent
+   ],
+   imports: [
+      BrowserModule,
+      NgbModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/app/newsEvents/newsEvents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newsEvents',
+  templateUrl: './newsEvents.component.html',
+  styleUrls: ['./newsEvents.component.css']
+})
+export class NewsEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
